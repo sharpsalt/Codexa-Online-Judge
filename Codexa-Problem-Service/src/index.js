b94a549ebfcc6fb316bc675f1a3d0cfaab2fd3dc
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-02-08 15:19:06 +0530

    Added ping fr checking service alive or not

--- Codexa-Problem-Service/src/index.js
+++ Codexa-Problem-Service/src/index.js
@@ -3,10 +3,16 @@ const express=require('express');
 const { PORT } = require('./config/server.config');
 const app=express();
 
+//Generally it's a good practice to actually add a route for checking the ping on th service whther thats ervie is alive or not 
+
+app.get('/ping',(req,res)=>{
+    return res.json({message:"Problem Serive is Alive"});
+});
+//if any point of time if you want to check if any service is alive or not then you can make check on ping service 
 
 app.listen(PORT,()=>{
     console.log(`Server started at ${PORT}`);
-})
+});
 
 
 

2c1abd95cfbe4cb9cb722745b38f96826f1cd7ae
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-03-06 13:27:10 +0530

    Feat: Implemented Internal Server Error

--- Codexa-Problem-Service/src/errors/badrequest.error.js
+++ Codexa-Problem-Service/src/errors/badrequest.error.js
@@ -4,8 +4,8 @@ const {StatusCodes}=require('http-status-codes')
 
 //so everyBadRequest is going to have common name 
 class BadRequest extends BaseError{
-    constructor(propertyname){
-        super("BadRequest",StatusCodes.BadRequest,`Invalid Structure for ${propertyname} provider`)
+    constructor(propertyname,details){
+        super("BadRequest",StatusCodes.BadRequest,`Invalid Structure for ${propertyname} provider`,details);
         //Every bad request can have a common description so ye kaha se aayega 
         //so our Constructor woll take propertyName, so 
         //let's say if somebody sends a request with empty description so we will send him a BadRequest (400 Status Code) like it is a client side error to 

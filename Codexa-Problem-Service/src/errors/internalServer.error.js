ba477e55d97db0ef8f1599b44a64585178f1b90d
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-03-06 13:29:37 +0530

    Fix: fixed Internal server error

--- Codexa-Problem-Service/src/errors/internalServer.error.js
+++ Codexa-Problem-Service/src/errors/internalServer.error.js
@@ -3,7 +3,7 @@ const {StatusCodes}=require('http-status-codes');
 
 class InternalServerError extends BaseError{
     constructor(details){
-        super("BadRequest",StatusCodes.INTERNAL_SERVER_ERROR,`Something went wrong`,details);
+        super("Internal Server Error",StatusCodes.INTERNAL_SERVER_ERROR,`Something went wrong`,details);
     }
 }
 

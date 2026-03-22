ce7697f6e739800939e2c2b9c5d52022e0ee314e
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-03-22 10:58:02 +0530

    Fix: Fixing DB cofig and adding db for testing and prod

--- Codexa-Problem-Service/src/config/db.config.js
+++ Codexa-Problem-Service/src/config/db.config.js
@@ -4,6 +4,10 @@ const { ATLAS_DB_URL } = require('./server.config');
 async function connectToDB(){
     try{
         await mongoose.connect(ATLAS_DB_URL);
+        // `
+        // What ideally should be here like if we are doing local dev setup then there should be different dev db 
+        // If we are working with Prod environment then theere should be difference database for that and for testing we need different database too
+        // `
     }catch(error){
         console.log("Unable to connect to the DB Server");
         console.log(error);

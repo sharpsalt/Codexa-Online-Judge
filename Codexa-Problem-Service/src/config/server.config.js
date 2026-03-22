8ead35a9eb092613448f27708378921a7ca76467
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-03-22 06:29:16 +0530

    feat: Imlementing server configs

--- Codexa-Problem-Service/src/config/server.config.js
+++ Codexa-Problem-Service/src/config/server.config.js
@@ -2,6 +2,7 @@ const dotenv=require('dotenv');
 dotenv.config();
 
 module.exports={
-    PORT:process.env.PORT || 3000
+    PORT:process.env.PORT || 3000,
+    ATLAS_DB_URL: process.env.ATLAS_DB_URL
 }
 

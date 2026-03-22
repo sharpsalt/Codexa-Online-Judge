b02824c962910fd2e106236414fc06e1c7944aa1
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-03-23 02:49:01 +0530

    feat: Implementing Schema for database

--- Codexa-Problem-Service/src/models/problem.model.js
+++ Codexa-Problem-Service/src/models/problem.model.js
@@ -11,15 +11,26 @@ const problemSchema=new Schema({
         required:[true,"Title cannot be empty"]
     },
     description:{
-         
-
-        
+        type:String,
+        required:[true,"Desciption cannot be empty"]
+    },
+    difficulty:{
+        type:String,
+        //ye enum se define krenge to jayda sahi hoga sb ke liye 
+        enum:['easy','medium','hard'], 
+        required: [true,'Difficulty cannot be empty'],
+        default:'easy'
     }
 });
 /*
 We can actually do validation as well like whenever your were defining a schema
-whatever property you have assign a objec and define the corresponding set of validation and then do rest
+whatever property you have assign a object and define the corresponding set of validation and then do rest
 
+whenever we try to create a branch new problem using problem model and we donot padd the title then ongoose will throw error
+For storing images we will use markdown,else w e can use coudinary as well or we could also use amazon aws s3
+
+
+Tag bhi lagaskte hai ismein 
 */
 
 

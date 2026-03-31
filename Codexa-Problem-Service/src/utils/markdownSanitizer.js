20ac9ff8a5ba365720478d43703f0617db29c50d
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-03-31 23:19:04 +0530

    docs: logic for implementing turndown pacage also

--- Codexa-Problem-Service/src/utils/markdownSanitizer.js
+++ Codexa-Problem-Service/src/utils/markdownSanitizer.js
@@ -19,6 +19,14 @@ module.exports=sanitizeMarkdown;
 
 //we donot need any script tag to comeup
 
+/*
+There is another package called as turndown, which tell us about the sanitize HTML to markdown
+altoug we can make sure that people are inputting markdown, and you're saving it in form of html 
+so while editing the problem the  person needs to know the HTML, and also wriitng HTML is pretty slower so 
+yes this turndown package is also very useful 
+
+*/
+
 
 
 

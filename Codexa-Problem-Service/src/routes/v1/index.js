3e06ca19013cb5b039e6286a82de263298ba1539
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-04-01 13:58:40 +0530

    fix: addressed v1 as of now

--- Codexa-Problem-Service/src/routes/v1/index.js
+++ Codexa-Problem-Service/src/routes/v1/index.js
@@ -4,8 +4,8 @@ const problemRouter=require('./problems.routes');
 
 const v1Router=express.Router();
 
-//if any request comes and route continues with /problems, we map it to ProblemROuter
-v1Router.use('/problems',problemRouter);
+//if any request comes and route continues with /problem, we map it to ProblemRouter
+v1Router.use('/problem',problemRouter);
 
 module.exports=v1Router;
 

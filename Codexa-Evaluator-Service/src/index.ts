5d362079420f4411ea80a6a78af7a0765eecf412
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-04-23 23:59:16 +0530

    feat: Added API Router and sample worker for message queue

--- Codexa-Evaluator-Service/src/index.ts
+++ Codexa-Evaluator-Service/src/index.ts
@@ -1,11 +1,25 @@
 import express,{Express} from "express";
 import serverConfig from "./config/serverConfig.js";
+import apirouter from "./routes/index.js";
+import sampleQueueProducer from "./producers/sampleQueueProducer.js";
+import sampleWorker from "./workers/sampleWorker.js";
+//All the custom imports(Imports which is created by me) is seperated, as tum usme bhi dekh skte ho
 
 const app:Express=express();
+
+app.use('/api',apirouter);
 
 app.listen(serverConfig.PORT,()=>{
     console.log(`Server started at ${serverConfig.PORT}`);
     // console.log("Chalne laga basically");
+    sampleWorker('SampleQueue');
+
+    sampleQueueProducer('SampleJob',{
+        name:"Srijan",
+        company:"Google",
+        position:"SDE 3",
+        location: "Remote | BLR | London"
+    });
 })
 /*
 There is a flag as --kill-others

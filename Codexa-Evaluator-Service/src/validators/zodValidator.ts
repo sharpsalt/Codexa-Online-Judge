fbd5c7f731ee0ad3c118612577041cd3d6f7d73f
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-06-23 01:15:33 +0530

    fix: Added return type as void, as earlier was missing

--- Codexa-Evaluator-Service/src/validators/zodValidator.ts
+++ Codexa-Evaluator-Service/src/validators/zodValidator.ts
@@ -9,7 +9,7 @@ import { ZodSchema } from 'zod';//yaha zod ko schema type import krrhe hai
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 //ye function parameter lega schemo ko which is ot tyoe ZodSchema<any>, mtlb koi bhi zod schema pass krdo
 //aur ye function basically ek function return krrha hai aisa smjho
-export const validate = (schema: ZodSchema<any>) => (req: Request, res: Response, next: NextFunction) => {
+export const validate = (schema: ZodSchema<any>) => (req: Request, res: Response, next: NextFunction): void => {
     try {
         schema.parse({
             ...req.body//spread operator
@@ -19,7 +19,7 @@ export const validate = (schema: ZodSchema<any>) => (req: Request, res: Response
 
     } catch (error) {
         console.log(error);
-        return res.status(400).json({
+        res.status(400).json({
             success: false,
             message: 'Invalid request params received',
             data: {},
